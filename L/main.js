f4e97bcd483764af685916f25290cc066a501233
Dirakon <609700zxc@mail.ru>
2021-03-27 01:21:50 +0500

    Made 'L' and fixed 'SPACE'

--- L/main.js
+++ L/main.js
@@ -1,4 +1,24 @@
-import modelPlayer from 'js-3d-model-viewer';
-const viewerElement = document.getElementById('viewer');
-const scene = modelPlayer.prepareScene(viewerElement);
-modelPlayer.loadObject(scene, './assets/sample.obj');
+
+function checkTextSize(divHeight,div,interval,fatherHeight){
+    let lineHeight = parseInt(div.style.lineHeight);
+    let maxLines = parseInt(fatherHeight/lineHeight);
+    let lines = divHeight / lineHeight;
+    if (lines >=maxLines){
+        div.setAttribute('contenteditable','false');
+        clearInterval(interval);
+    }
+}
+
+
+let hor = document.querySelector('.hor');
+let horFather = document.querySelector('.smallBox');
+let horInterval = setInterval(function () {
+    checkTextSize(hor.offsetHeight,hor,horInterval,horFather.clientHeight);
+},10);
+
+let vert = document.querySelector('.vert');
+let vertFather = document.querySelector('.bigBox');
+let vertInterval = setInterval(function () {
+    checkTextSize(vert.offsetWidth,vert,vertInterval,vertFather.clientWidth);
+
+},10);
--- SPACE/main.js
+++ SPACE/main.js
@@ -1,7 +1,10 @@
 
 let img =document.querySelector('img');
-let center = [window.screen.availHeight/2,window.screen.availWidth/2];
-center[0]-=img.height/2;
-center[1]-=img.width/2;
-img.style.top = center[0].toString() + 'px';
-img.style.left=center[1].toString() + 'px';
+let center = [window.innerHeight/2,window.innerWidth/2];
+img.onload = function() {
+    center[0]-=img.height/2;
+    center[1]-=img.width/2;
+    img.style.top = center[0].toString() + 'px';
+    img.style.left=center[1].toString() + 'px';
+    img.style.visibility='visible';
+}

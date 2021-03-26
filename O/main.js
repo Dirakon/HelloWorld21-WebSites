e25abcd885646851823d08cc5909e11e9a0fdeca
Dirakon <609700zxc@mail.ru>
2021-03-26 20:16:55 +0500

    Commented 'O' and made line thicker

--- O/main.js
+++ O/main.js
@@ -1,9 +1,14 @@
 
+// Переменная, позволяющая отличить четные нажатия от нечетных
 clickDone = false
 
+// Функция, обрабатывающая клики
 function handle(event){
+    // Делаем линию выделяющейся
     ctx.strokeStyle = 'black';
-    ctx.lineWidth = 1;
+    ctx.lineWidth = 5;
+
+    // Рисуем линию или ставим точку в зависимости от четности клика
     if (!clickDone){
         ctx.moveTo(event.clientX, event.clientY);
         clickDone = true
@@ -13,15 +18,19 @@ function handle(event){
         clickDone = false
     }
 }
+// Находим полотно и настраиваем его
 const canvas = document.querySelector('.myCanvas');
 const width = window.innerWidth;
 const height =  window.innerHeight;
+
+// Настраиваем контекст полотна
 const ctx = canvas.getContext('2d');
 ctx.canvas.width = width;
 ctx.canvas.height=height;
 
-
+// Делаем фоновый цвет
 ctx.fillStyle = 'wheat';
 ctx.fillRect(0,0,width,height);
 
+// Соглашаемся принимать клики
 document.body.onclick=handle;
